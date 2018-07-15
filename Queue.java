44e6715305beca5f9bdb90fc8a297b55c0c9b826
krishremya <krishremya99@gmail.com>
2018-07-15 14:44:58 +0530

    Basic-Data-Structures

--- Queue.java
+++ Queue.java
@@ -1,29 +1,39 @@
+/*The program is to implement queue. TO input the elements to the front of the queue and to delete it.*/
+
 import java.util.*;
 
 public class Queue {
       int front, rear, size ;
       static int maximum = 1000;
       int [] A = new int[maximum];
+
+      //constructor to initialize the value of the front and the rear end of the queue
       Queue() {
           front = 0;
           rear = maximum-1;
 
       }
 
+     //Function to find if the queue is full
      public boolean isFull(Queue q) {
        return (q.size == maximum);
 
      }
 
+     //Function to find if the queue is empty
      public boolean isEmpty(Queue q) {
         return (q.size==0);
 
      }
 
+     //Function to insert the elements
      public void enQueue(int data) {
 
+        //Checking if the queue is full
          if(isFull(this))
             return ;
+
+         //Adding elements to the rear part there by increasing the size of the queue
          this.rear = (this.rear + 1)%maximum;
          this.A[this.rear] = data;
          this.size = this.size+1;
@@ -31,6 +41,7 @@ public class Queue {
 
      }
 
+     //Deleting the elements from front of the queue
      public int deQueue() {
        if(isEmpty(this))
           System.out.println("Queue Underflow! ");
